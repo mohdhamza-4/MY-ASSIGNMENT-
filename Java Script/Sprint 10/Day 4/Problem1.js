0d2a087b8975ed01595f5ecdc9a4ea0bae180dfa
mohdhamza-4 <hamzamohd2572006@gmai.com>
2024-08-05 22:42:17 +0530

    Done

---
+++ Java Script/Sprint 10/Day 4/Problem1.js
@@ -0,0 +1,64 @@
+var a = 25
+
+
+console.log(a**2)
+
+
+ a = 5
+ b = 8
+ c = 9
+ d = 4
+ e = 3
+
+
+result = a*b**3 + b/d*c + 2*a*b*c
+
+
+console.log(result)
+
+
+var a = 20
+var b = 40
+
+
+console.log(`Before swap: a = ${a}, b = ${b}`);
+
+
+// Swap values using arithmetic operations
+a = a + b;
+b = a - b;
+a = a - b;
+
+
+console.log(`After swap: a = ${a}, b = ${b}`);
+
+
+
+
+var C = 50
+
+
+F = (C * 9/5) + 32
+
+
+console.log(F)
+
+
+L = 8
+w = 5
+
+
+c = L*w
+
+
+console.log(c)
+
+
+var a = 10
+
+
+c = a % 5
+
+
+console.log(c)
+
